095b72c3e9eaf91f6b1f59d7c7402c3ce09527d0
JUSTKims <KimTG0501@gmail.com>
2021-06-02 20:06:26 +0900

    plz..

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Member(models.Model):
        email = models.EmailField(max_length=30)
-       name = models.CharField(max_length=15)
+       name = models.CharField(max_length=20)
        password = models.IntegerField(default=8)
        phone_number = models.IntegerField()
      
